0fd94ca6fb2bea7b89dd6fcf30a363aaa5fb1403
segmue <sebigmuer@gmail.com>
2025-04-08 22:41:09 +0200

    well, did a lot I think.. Added empty POI Window & Menu when rightklicking in map,added POI-Markers which color is poi.status dependent, added first draft of POIlist, refactored VerticalSidebar to componend and did some debugging

---
+++ app/types/poi.tsx
@@ -0,0 +1,36 @@
+import type { Point } from "geojson";
+
+export interface PointOfInterest {
+    poiId: number;
+    name: string;
+    coordinate: Point;
+    description: string;
+    category: PoiCategory;
+    priority: PoiPriority;
+    creatorId: number;
+    status: PoiAcceptanceStatus;
+    upvotes: number; // temporary solution, not sure if a list of user_ids is needed here...
+    downvotes: number;
+    eligibleVoteCount: number;
+}
+
+export enum PoiCategory {
+    SIGHTSEEING = "SIGHTSEEING",
+    FOOD = "FOOD",
+    ACCOMMODATION = "ACCOMMODATION",
+    ACTIVITY = "ACTIVITY",
+    OTHER = "OTHER"
+}
+
+export enum PoiPriority {
+    HIGH = "HIGH",
+    MEDIUM = "MEDIUM",
+    LOW = "LOW",
+    OPTIONAL = "OPTIONAL"
+}
+
+export enum PoiAcceptanceStatus {
+    PENDING = "PENDING",
+    APPROVED = "APPROVED",
+    REJECTED = "REJECTED"
+}
