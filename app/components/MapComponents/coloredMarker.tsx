7792f4202fdf672bbbcb8f1866864775be16061e
segmue <sebigmuer@gmail.com>
2025-05-05 08:59:23 +0200

    refined marker colors

--- app/components/MapComponents/coloredMarker.tsx
+++ app/components/MapComponents/coloredMarker.tsx
@@ -2,15 +2,20 @@ import {Icon} from "leaflet";
 
 export function ColoredMarker(color: string): Icon {
     const svgString = `
-    <svg width="35" height="35" viewBox="0 0 35 35" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <path fill-rule="evenodd" clip-rule="evenodd" d="M7.01639 21.8641L17.125 34.5L27.2337 21.8641C29.1862 19.4235 30.25 16.3909 30.25 13.2652V12.625C30.25 5.37626 24.3737 -0.5 17.125 -0.5C9.87626 -0.5 4 5.37626 4 12.625V13.2652C4 16.3909 5.06378 19.4235 7.01639 21.8641ZM17.125 17C19.5412 17 21.5 15.0412 21.5 12.625C21.5 10.2088 19.5412 8.25 17.125 8.25C14.7088 8.25 12.75 10.2088 12.75 12.625C12.75 15.0412 14.7088 17 17.125 17Z" fill="${color}"/>
-    </svg>
-  `;
+<svg width="35" height="65" viewBox="-5 -5 45 65" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <defs>
+    <filter id="glow" x="-50%" y="-50%" width="200%" height="200%">
+      <feDropShadow dx="0" dy="0" stdDeviation="4" flood-color="white" flood-opacity="0.4"/>
+    </filter>
+  </defs>
+  <path fill-rule="evenodd" clip-rule="evenodd" d="M7.01639 21.8641L17.125 34.5L27.2337 21.8641C29.1862 19.4235 30.25 16.3909 30.25 13.2652V12.625C30.25 5.37626 24.3737 -0.5 17.125 -0.5C9.87626 -0.5 4 5.37626 4 12.625V13.2652C4 16.3909 5.06378 19.4235 7.01639 21.8641ZM17.125 17C19.5412 17 21.5 15.0412 21.5 12.625C21.5 10.2088 19.5412 8.25 17.125 8.25C14.7088 8.25 12.75 10.2088 12.75 12.625C12.75 15.0412 14.7088 17 17.125 17Z" stroke="white" stroke-width="1.2" filter="url(#glow)" fill="${color}" shape-rendering="geometricPrecision"/>
+</svg>
+`;
     const svgUrl = `data:image/svg+xml;base64,${btoa(svgString)}`;
 
     return new Icon({
         iconUrl: svgUrl,
-        iconSize: [32, 32],
-        iconAnchor: [16, 32],
+        iconSize: [48, 48],
+        iconAnchor: [32, 32],
     });
 }
